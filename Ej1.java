6cf232d960fdffc504e158eae398b3008c50ab84
juanpabloochoa514 <76634910+juanpabloochoa514@users.noreply.github.com>
2021-10-03 16:52:00 -0300

    Update Ej1.java

--- Ej1.java
+++ Ej1.java
@@ -5,7 +5,7 @@ public class Ej1 {
          Scanner scanner=new Scanner(System.in);
          System.out.println("IMPRIMA UN NUMERO");
          int num1=scanner.nextInt();
-         System.out.println("IMPRIMA OTRO NUMERO");
+         System.out.println(" IMPRIMA OTRO NUMERO");
          int num2=scanner.nextInt();
          if(num1>num2){
              System.out.println("numero1 es mayor a numero 2");
